08160d1aeec9f5c858f6f965b3957c092069a766
Chitranshu-7 <chitranshuctr@gmail.com>
2023-06-30 00:33:10 +0530

    all

--- Index.js
+++ Index.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import TextSpeech from './src/ProjectIdea/TextSpeech'
+import App from './App'
 
 export default function Index() {
   return (
-    <TextSpeech></TextSpeech>
+   <App/>
 
   )
 }
